24f0528de477ff6bdba103568989383894dd3e47
Deivid-Coder12 <dorozcogo@unal.edu.co>
2025-11-07 20:03:49 -0500

    Add user registration and login functionality
    Refactor Flask application to include user registration and login routes.

--- app.py
+++ app.py
@@ -1,19 +1,56 @@
 # Inicio para usar flask
-"""
-from flask import Flask, render_template, request, jsonify
-from src.viewmodel.usuarios_viewmodel import UsuarioViewModel
-from src.viewmodel.grupos_viewmodel import GruposViewModel
+from flask import Flask, render_template, request, redirect, url_for, flash
+from src.viewmodel.auth_viewmodel import UsuarioAuthViewModel
+from src.services.firebase import FirebaseService
+import time
+from src.services.firebase_global import firebase_global
 
+auth_vm = UsuarioAuthViewModel()
 app = Flask(__name__)
+app.secret_key = "supersecretkey"
 
-# Instancias del ViewModel
-usuario_vm = UsuarioViewModel()
-grupo_vm = GruposViewModel()
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-@app.route('/')
-def inicio():
-    return "¡Bienvenido al sistema de grupos UNAL!"
+@app.route("/registro", methods=["GET", "POST"])
+def registro_usuario():
+    if request.method == "POST":
+        nombre = request.form["name"]
+        correo = request.form["correo"]
+        password = request.form["password"]
+        carrera = request.form["carrera"]
+        resultado = auth_vm.registrar_usuario(nombre, correo, password, carrera)
+        if resultado["success"]:
+            flash("Registro exitoso", "success")
+            return redirect(url_for("index"))
+        else:
+            flash(resultado["error"], "danger")
+    return render_template("registrarse.html")
 
-if __name__ == '__main__':
+
+
+@app.route("/inicio_sesion", methods=["GET", "POST"])
+def inicio_sesion():
+    if request.method == "POST":
+        correo = request.form["correo"]
+        password = request.form["password"]
+        resultado = auth_vm.iniciar_sesion(correo, password)
+        if resultado["success"]:
+            flash("Inicio de sesión exitoso", "success")
+            return redirect(url_for("index"))
+        else:
+            flash(resultado["error"], "danger")
+    return render_template("iniciar_sesion.html")
+
+def main():
     app.run(debug=True)
-"""
+    registro_usuario()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
